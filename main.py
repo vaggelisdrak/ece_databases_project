91bd2fe8b0598cb03f23587899ecb18d93f314a6
emmanouelmougios <mougiosman@gmail.com>
2023-12-18 20:33:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ class GUI():
         for i, row in enumerate(games_data):
             if row[1] == 'REGULAR SEASON':
                 new_row = row[:1] + row[2:4] + row[5:]  # Create a new tuple without the second and fifth elements
-                row_str = f" {new_row[0]}\n {new_row[1]}   {new_row[3]}\n {new_row[2]}   {new_row[4]}\n"
+                row_str = f" {new_row[0]}\n {new_row[1]}\t\t{new_row[3]}\n {new_row[2]}\t\t{new_row[4]}\n"
             else:
                 row_str = ', '.join(map(str, row))
 
@@ -199,6 +199,58 @@ class GUI():
 
 
     def show_standings(self):
+        
+        # Connect to the SQLite database
+        connection = sqlite3.connect('project_db.db')
+        cursor = connection.cursor()
+
+        # Your SQL query
+        sql_query_east = """
+        SELECT team, SUM(win) AS Wins, SUM(loss) AS Losses 
+        FROM (
+            SELECT Νικητής AS team, COUNT(*) AS win, 0 AS loss, 'Όνομα', id_group
+            FROM ΑΓΩΝΑΣ, ΟΜΑΔΑ
+            WHERE ΟΜΑΔΑ.'Όνομα' = Νικητής AND ΟΜΑΔΑ.id_group = 1
+            GROUP BY Νικητής
+            UNION ALL
+            SELECT Ηττημένος AS team, 0 AS win, COUNT(*) AS loss, 'Όνομα', id_group
+            FROM ΑΓΩΝΑΣ, ΟΜΑΔΑ
+            WHERE ΟΜΑΔΑ.'Όνομα' = Ηττημένος AND ΟΜΑΔΑ.id_group = 1
+            GROUP BY Ηττημένος
+        ) 
+        GROUP BY team
+        ORDER BY Wins DESC
+        """
+        sql_query_west = """
+        SELECT team, SUM(win) AS Wins, SUM(loss) AS Losses 
+        FROM (
+            SELECT Νικητής AS team, COUNT(*) AS win, 0 AS loss, 'Όνομα', id_group
+            FROM ΑΓΩΝΑΣ, ΟΜΑΔΑ
+            WHERE ΟΜΑΔΑ.'Όνομα' = Νικητής AND ΟΜΑΔΑ.id_group = 2
+            GROUP BY Νικητής
+            UNION ALL
+            SELECT Ηττημένος AS team, 0 AS win, COUNT(*) AS loss, 'Όνομα', id_group
+            FROM ΑΓΩΝΑΣ, ΟΜΑΔΑ
+            WHERE ΟΜΑΔΑ.'Όνομα' = Ηττημένος AND ΟΜΑΔΑ.id_group = 2
+            GROUP BY Ηττημένος
+        ) 
+        GROUP BY team
+        ORDER BY Wins DESC
+        """
+
+        # Execute the query
+        cursor.execute(sql_query_east)
+        east_data = cursor.fetchall()  # Fetch all results
+        
+        cursor.execute(sql_query_west)
+        west_data = cursor.fetchall()  # Fetch all results
+
+        print(east_data)
+        print(west_data)
+
+        # Close the connection
+        connection.close()
+        
          # Remove the old text widget if it exists
         if hasattr(self, 'text_widget'):
             self.text_widget.destroy()
@@ -208,6 +260,30 @@ class GUI():
             self.text_widget2.destroy()
         if hasattr(self, 'frame'):
             self.frame.destroy()
+            
+        # Create a new frame widget
+        self.frame = Frame(self.widget)
+        self.frame.pack()
+            
+        # Create two new text widgets
+        self.text_widget1 = Text(self.frame, width=50, height=30, bg='#333', fg='white', font=('Arial', 12))
+        self.text_widget2 = Text(self.frame, width=50, height=30, bg='#333', fg='white', font=('Arial', 12))
+
+        # Place the text widgets in the frame using a grid layout
+        self.text_widget1.grid(row=0, column=0)
+        self.text_widget2.grid(row=0, column=1)
+        
+        self.text_widget1.tag_configure("bold", font=("Arial", 14, "bold"))
+        self.text_widget2.tag_configure("bold", font=("Arial", 14, "bold"))
+
+        self.text_widget1.insert('end', '\n '+ 'EASTERN CONFERENCE' + '\n\n', "bold")
+        for row in east_data:
+            self.text_widget1.insert('end', ' '+ row[0] + '\t\t\t' + str(row[1]) + '-' +  str(row[2]) +'\n\n')
+            
+
+        self.text_widget2.insert('end', '\n '+ 'WESTERN CONFERENCE' + '\n\n', "bold")
+        for row in west_data:
+            self.text_widget2.insert('end', ' '+ row[0] + '\t\t\t' + str(row[1]) + '-' +  str(row[2]) +'\n\n')
 
     def show_stats(self):
         # Connect to the SQLite database and fetch teams data
