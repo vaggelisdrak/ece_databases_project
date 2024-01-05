dc61e87ae3c157ab29842afe8984212b943808b7
emmanouelmougios <mougiosman@gmail.com>
2024-01-05 18:12:41 +0200

    remove comments

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ class GUI():
         self.menu = Frame(self.widget, bg='#333', bd=5)
         self.menu.pack(side='top', fill='x')
 
-        # Create four buttons on top of each other within the 'widget' frame
+        # Create four buttons
         button_texts = ['ΑΓΩΝΕΣ', 'ΒΑΘΜΟΛΟΓΙΕΣ', 'ΟΜΑΔΕΣ', 'ΣΤΑΤΙΣΤΙΚΑ']
         for text in button_texts:
             button = Button(self.menu, text=text, height=4, font='ariel 15 bold', command=lambda t=text: self.show_window(t))
@@ -75,11 +75,11 @@ class GUI():
             self.show_stats()
 
     def show_games(self):
+        
         # Connect to the SQLite database
         connection = sqlite3.connect('project_db.db')
         cursor = connection.cursor()
 
-        # Your SQL query
         sql_query = """
         SELECT a.Ημερομηνία, a.Είδος, p.'HOME TEAM', p.'AWAY TEAM ', p.Γήπεδο, p.'Points ΗΟΜΕ', p.'Points AWAY'
         FROM ΑΓΩΝΑΣ AS a, Παίζει AS p
@@ -88,13 +88,11 @@ class GUI():
         ORDER BY a.Ημερομηνία
         """
 
-        # Execute the query
         cursor.execute(sql_query)
-        games_data = cursor.fetchall()  # Fetch all results
+        games_data = cursor.fetchall() 
 
         print(games_data)
 
-        # Close the connection
         connection.close()
         
         # Remove the old text widgets if they exist
@@ -111,40 +109,38 @@ class GUI():
         self.frame = Frame(self.widget)
         self.frame.pack()
 
-        # Convert date from MM/DD/YYYY to YYYY-MM-DD and reverse the list
+        # Convert date from MM/DD/YYYY to YYYY-MM-DD and sort the results so the games can be printed with date order 
         games_data = sorted([(datetime.strptime(row[0], '%m/%d/%Y').strftime('%Y-%m-%d'),) + row[1:] for row in games_data], reverse=True)
 
-        # Create two new text widgets
+        # Create two widgets
         self.text_widget1 = Text(self.frame, width=50, height=30, bg='#333', fg='white', font=('Arial', 12))
         self.text_widget2 = Text(self.frame, width=50, height=30, bg='#333', fg='white', font=('Arial', 12))
 
-        # Place the text widgets in the frame using a grid layout
         self.text_widget1.grid(row=0, column=0)
         self.text_widget2.grid(row=0, column=1)
 
         for i, row in enumerate(games_data):
-            # Convert date from 'YYYY-MM-DD' to 'MM/DD/YYYY'
+            # Again convert the date from 'YYYY-MM-DD' to 'MM/DD/YYYY'
             formatted_date = datetime.strptime(row[0], '%Y-%m-%d').strftime('%m/%d/%Y')
             
             if row[1] == 'REGULAR SEASON':
-                new_row = (formatted_date,) + row[2:4] + row[5:] # Create a new tuple without the second and fifth elements
+                new_row = (formatted_date,) + row[2:4] + row[5:]
                 row_str = f" {new_row[0]}\n {new_row[1]}\t\t{new_row[3]}\n {new_row[2]}\t\t{new_row[4]}\n"
             else:
-                new_row = (formatted_date,) + row[2:4] + row[5:] # Create a new tuple without the second and fifth elements
+                new_row = (formatted_date,) + row[2:4] + row[5:] 
                 row_str = f" {new_row[0]}\n {new_row[1]}\t\t{new_row[3]}\n {new_row[2]}\t\t{new_row[4]}\n"
 
-            # Insert the row into the first text widget if i is even, otherwise insert it into the second text widget
+            # Place the games into the widgets
             if i % 2 == 0:
                 self.text_widget1.insert('end', row_str + '\n')
             else:
                 self.text_widget2.insert('end', row_str + '\n')
 
     def show_teams(self):
-        # Connect to the SQLite database and fetch teams data
+        
         connection = sqlite3.connect('project_db.db')
         cursor = connection.cursor()
 
-        # Your SQL query
         sql_query = """
         SELECT om.Όνομα AS "ΟΜΑΔΑ", p.Ονοματεπώνυμο
         FROM ΟΜΑΔΑ AS om
@@ -152,16 +148,13 @@ class GUI():
         ON om.id_team = p.id_team
         """
 
-        # Execute the query
         cursor.execute(sql_query)
-        teams_data = cursor.fetchall()  # Fetch all results
+        teams_data = cursor.fetchall()  
 
         print(teams_data)
 
-        # Close the connection
         connection.close()
         
-        # Remove the old text widgets if they exist
         if hasattr(self, 'text_widget'):
             self.text_widget.destroy()
         if hasattr(self, 'text_widget1'):
@@ -171,45 +164,42 @@ class GUI():
         if hasattr(self, 'frame'):
             self.frame.destroy()
 
-        # Create a new frame widget
         self.frame = Frame(self.widget)
         self.frame.pack()
 
-        team_widgets = {}  # Create an empty dictionary to store the text widgets for each team
-        team_count = 0  # Initialize a counter for the number of teams
+        team_widgets = {}  # widgets for each team
+        team_count = 0  # counter for the number of teams
 
         for row in teams_data:
-            team_name = row[0]  # Get the team name
-            team_player = row[1]  # Get the player name
+            team_name = row[0]  
+            team_player = row[1]  
 
-            # If the team name is not in the dictionary, create a new text widget for it
+            # If the team name is not in the team_widgets, create a new widget for it
             if team_name not in team_widgets:
                 text_widget = Text(self.frame, width=25, height=11, bg='#333', fg='white', font=('Arial', 12))
-                text_widget.grid(row=team_count // 6, column=team_count % 6)  # Place the text widget in the appropriate row and column
+                text_widget.grid(row=team_count // 6, column=team_count % 6)  
                 text_widget.tag_configure("bold", font=("Arial", 14, "bold"))
                 text_widget.insert('end', ' ' + team_name + '\n\n', "bold")
-                team_widgets[team_name] = text_widget  # Add the text widget to the dictionary
-                team_count += 1  # Increment the counter for the number of teams
+                team_widgets[team_name] = text_widget  
+                team_count += 1  
 
-            # Insert the player name into the text widget for the team
             team_widgets[team_name].insert('end', ' '+ team_player + '\n')
         
-            # New code to fill remaining cells with gray widgets
             rows = team_count // 6
-            if team_count % 6 != 0:  # If there are any teams in the last row
-                rows += 1  # Add one to the number of rows
+            if team_count % 6 != 0: 
+                rows += 1  
 
-            for i in range(team_count, rows * 6):  # For each remaining cell in the grid
-                text_widget = Text(self.frame, width=25, height=11, bg='#333', fg='white', font=('Arial', 12))  # Create a gray text widget
-                text_widget.grid(row=i // 6, column=i % 6)  # Place the text widget in the appropriate row and column
+            for i in range(team_count, rows * 6):  
+                text_widget = Text(self.frame, width=25, height=11, bg='#333', fg='white', font=('Arial', 12))  
+                text_widget.grid(row=i // 6, column=i % 6)  
 
     def show_standings(self):
         
-        # Connect to the SQLite database
         connection = sqlite3.connect('project_db.db')
         cursor = connection.cursor()
 
-        # Your SQL query
+        # queries for the rankings of the teams for each conference
+        
         sql_query_east = """
         SELECT team, SUM(win) AS Wins, SUM(loss) AS Losses 
         FROM (
@@ -243,20 +233,17 @@ class GUI():
         ORDER BY Wins DESC
         """
 
-        # Execute the query
         cursor.execute(sql_query_east)
-        east_data = cursor.fetchall()  # Fetch all results
+        east_data = cursor.fetchall()  
         
         cursor.execute(sql_query_west)
-        west_data = cursor.fetchall()  # Fetch all results
+        west_data = cursor.fetchall()  
 
         print(east_data)
         print(west_data)
 
-        # Close the connection
         connection.close()
         
-         # Remove the old text widget if it exists
         if hasattr(self, 'text_widget'):
             self.text_widget.destroy()
         if hasattr(self, 'text_widget1'):
@@ -266,15 +253,12 @@ class GUI():
         if hasattr(self, 'frame'):
             self.frame.destroy()
             
-        # Create a new frame widget
         self.frame = Frame(self.widget)
         self.frame.pack()
             
-        # Create two new text widgets
         self.text_widget1 = Text(self.frame, width=50, height=30, bg='#333', fg='white', font=('Arial', 12))
         self.text_widget2 = Text(self.frame, width=50, height=30, bg='#333', fg='white', font=('Arial', 12))
 
-        # Place the text widgets in the frame using a grid layout
         self.text_widget1.grid(row=0, column=0)
         self.text_widget2.grid(row=0, column=1)
         
@@ -291,11 +275,10 @@ class GUI():
             self.text_widget2.insert('end', ' '+ row[0] + '\t\t\t' + str(row[1]) + '-' +  str(row[2]) +'\n\n')
 
     def show_stats(self):
-        # Connect to the SQLite database and fetch teams data
+
         connection = sqlite3.connect('project_db.db')
         cursor = connection.cursor()
         
-        # Remove the old text widgets if they exist
         if hasattr(self, 'text_widget'):
             self.text_widget.destroy()
         if hasattr(self, 'text_widget1'):
@@ -305,11 +288,10 @@ class GUI():
         if hasattr(self, 'frame'):
             self.frame.destroy()
             
-        # Create a new text widget and insert the data
         self.text_widget = Text(self.widget, width=100, height=50, bg='#333', fg='white', font=('Arial', 12))
         self.text_widget.pack()
 
-        # Your SQL query FOR PPG ---------------------------------------------
+        # query FOR PPG ---------------------------------------------
         sql_query = """
         SELECT P.Ονοματεπώνυμο, AVG(A.Points) AS PPG
         FROM "Αγωνίζεται " AS A, ΠΑΙΚΤΗΣ AS P
@@ -319,9 +301,8 @@ class GUI():
         LIMIT 5
         """
 
-        # Execute the query FOR PPG
         cursor.execute(sql_query)
-        ppg_standings = cursor.fetchall()  # Fetch all results
+        ppg_standings = cursor.fetchall()  
 
         print(ppg_standings)
         
@@ -332,7 +313,7 @@ class GUI():
             row_str = ', '.join(map(str, row))
             self.text_widget.insert('end', ' '+ row_str + '\n')
 
-        # Your SQL query RPG --------------------------------------------------
+        # query RPG --------------------------------------------------
         sql_query = """
         SELECT P.Ονοματεπώνυμο, AVG(A.Rebounds) AS RPG
         FROM "Αγωνίζεται " AS A, ΠΑΙΚΤΗΣ AS P
@@ -342,9 +323,8 @@ class GUI():
         LIMIT 5
         """
 
-        # Execute the query FOR RPG
         cursor.execute(sql_query)
-        rpg_standings = cursor.fetchall()  # Fetch all results
+        rpg_standings = cursor.fetchall()  
 
         print(rpg_standings)
         
@@ -355,7 +335,7 @@ class GUI():
             row_str = ', '.join(map(str, row))
             self.text_widget.insert('end', ' '+ row_str + '\n')
 
-        # Your SQL query FOR APG -------------------------------------------------
+        # query FOR APG -------------------------------------------------
         sql_query = """
         SELECT P.Ονοματεπώνυμο, AVG(A.Assists) AS APG
         FROM "Αγωνίζεται " AS A, ΠΑΙΚΤΗΣ AS P
@@ -365,9 +345,8 @@ class GUI():
         LIMIT 5
         """
 
-        # Execute the query
         cursor.execute(sql_query)
-        apg_standings = cursor.fetchall()  # Fetch all results
+        apg_standings = cursor.fetchall()  
 
         print(apg_standings)
         
@@ -378,7 +357,6 @@ class GUI():
             row_str = ', '.join(map(str, row))
             self.text_widget.insert('end', ' '+ row_str + '\n')
 
-        # Close the connection
         connection.close()
 
   
